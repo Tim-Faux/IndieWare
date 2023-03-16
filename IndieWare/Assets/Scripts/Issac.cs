e08be9d390d9f11ce1fca1258ac599c2359378f5
DESKTOP-NG7FCV0\Tim <tim-faux@comcast.net>
2023-03-15 20:19:18 -0400

    Limit Issac's movement to three distinct spots

--- IndieWare/Assets/Scripts/Issac.cs
+++ IndieWare/Assets/Scripts/Issac.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Issac : MonoBehaviour
 {
+	int IssacPos = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,11 +14,13 @@ public class Issac : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-		if (Input.GetKeyDown(KeyCode.A)) {
+		if (Input.GetKeyDown(KeyCode.A) && IssacPos > -1) {
 			transform.position = new Vector2(transform.position.x - 5, transform.position.y);
+			IssacPos--;
 		}
-		if (Input.GetKeyDown(KeyCode.D)) {
+		if (Input.GetKeyDown(KeyCode.D) && IssacPos < 1) {
 			transform.position = new Vector2(transform.position.x + 5, transform.position.y);
+			IssacPos++;
 		}
 	}
 }
