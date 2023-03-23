9efad01f5daf40bd5030f78f114d3f311d01d550
DESKTOP-NG7FCV0\Tim <tim-faux@comcast.net>
2023-03-22 20:20:00 -0400

    Randomized Foot starting position

--- IndieWare/Assets/Scripts/MomsFoot.cs
+++ IndieWare/Assets/Scripts/MomsFoot.cs
@@ -6,13 +6,15 @@ public class MomsFoot : MonoBehaviour
 {
 	[SerializeField]
 	public const float TimeBetweenStomps = 3;
+	private const float FootStartXPos = 5;
+	private const float FootStartYPos = 9;
 	private float timeRemaining = TimeBetweenStomps;
 	private bool timerIsRunning = true;
 	// Start is called before the first frame update
 	void Start()
     {
-        
-    }
+		transform.position = SelectFootPos();
+	}
 
     // Update is called once per frame
     void Update()
@@ -28,5 +30,10 @@ public class MomsFoot : MonoBehaviour
 				//rerandomize foot position
 			}
 		}
+	}
+
+	public Vector2 SelectFootPos()
+	{
+		return new Vector2(FootStartXPos + (Random.Range(-1, 1) * 5), FootStartYPos);
 	}
 }
