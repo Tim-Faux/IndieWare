e710ee6d52ac4770b8549388c0b787b448ef85d0
DESKTOP-NG7FCV0\Tim <tim-faux@comcast.net>
2023-03-29 19:40:35 -0400

    foot smushes issac and resets position after a second

--- IndieWare/Assets/Scripts/MomsFoot.cs
+++ IndieWare/Assets/Scripts/MomsFoot.cs
@@ -6,31 +6,46 @@ public class MomsFoot : MonoBehaviour
 {
 	[SerializeField]
 	public const float TimeBetweenStomps = 3;
+	private const float LengthOfStomp = 1;
 	private const float FootMiddleXPos = 0;
 	private const float FootStartYPos = 9;
-	private float timeRemaining = TimeBetweenStomps;
+	private float timeRemaining;
+	private float stompTimeRemaining;
 	// Start is called before the first frame update
 	void Start()
     {
 		transform.position = SelectFootPos();
+		timeRemaining = TimeBetweenStomps;
+		stompTimeRemaining = LengthOfStomp;
 	}
 
     // Update is called once per frame
     void Update()
     {
+		var issac = GameObject.Find("Issac");
 		if (timeRemaining > 0) {
 			timeRemaining -= Time.deltaTime;
 		}
+		else if(issac != null && issac.transform.position.x == transform.position.x) {
+			transform.position = new Vector2(transform.position.x, 4.5f);
+			Destroy(issac);
+		}
 		else {
-			timeRemaining = TimeBetweenStomps;
-			transform.position = SelectFootPos();
+			transform.position = new Vector2(transform.position.x, 4.5f);
+			if (stompTimeRemaining > 0) {
+				stompTimeRemaining -= Time.deltaTime;
+			}
+			else {
+				stompTimeRemaining = LengthOfStomp;
+				timeRemaining = TimeBetweenStomps;
+				transform.position = SelectFootPos();
+			}
 		}
 	}
 
+	// Select one of 3 positions for the foot 
 	public Vector2 SelectFootPos()
 	{
-		var temp = Random.Range(-1, 2);
-		Debug.Log(temp);
-		return new Vector2(FootMiddleXPos + (temp * 5), FootStartYPos);
+		return new Vector2(FootMiddleXPos + (Random.Range(-1, 2) * 5), FootStartYPos);
 	}
 }
